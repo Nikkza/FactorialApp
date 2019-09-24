187b079e43e81ef4353361c11ecb1089e92bd910
Nikkza <isaksson.nicklas80@outlook.com>
2019-09-24 19:44:37 +0300

    made error handling

--- FactorialApp/Program.cs
+++ FactorialApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 
 namespace FactorialApp
 {
@@ -17,52 +16,51 @@ namespace FactorialApp
                 BigInteger getReturnedNumber = 0;
 
                 bool s = BigInteger.TryParse(value, out BigInteger number);
-                if (!string.IsNullOrEmpty(value) && s)
+                if (s && number >= 0 && number <= 10000)
                 {
-                    try
+                    getReturnedNumber = DisplayNumber(number);
+                    if (!getReturnedNumber.Equals(null))
                     {
-                        getReturnedNumber = DisplayNumber(number);
-                        if (!getReturnedNumber.Equals(null))
+                        string str = Convert.ToString(getReturnedNumber);
+                        try
                         {
-                            string str = Convert.ToString(getReturnedNumber);
-                            try
+                            if (str.Length <= 3 && !string.IsNullOrEmpty(str))
+                                Console.WriteLine(str);
+                            else
                             {
-                                if (str.Length <= 3 && !string.IsNullOrEmpty(str))
-                                    Console.WriteLine(str);
-                                else
-                                {
-                                    trimmedValue = str.TrimEnd('0');
-                                    subString = trimmedValue.Substring(trimmedValue.Length - 3);
-                                    Console.WriteLine(subString);
-                                }
-                            }
-                            catch (Exception err)
-                            {
-                                throw err;
+                                trimmedValue = str.TrimEnd('0');
+                                subString = trimmedValue.Substring(trimmedValue.Length - 3);
+                                Console.WriteLine(subString);
                             }
                         }
+                        catch (Exception err)
+                        {
+                            throw err;
+                        }
+                    }
 
-                    }
-                    catch (InsufficientExecutionStackException ex)
-                    {
-                        Console.WriteLine("Out of limit");
-                    }
                 }
                 else
-                    Console.WriteLine("Value can not be(A - Ö) Number must start from (0)");
+                    Console.WriteLine($"Value can not be(A - Ö) Number must start from (0) " +
+                        $"or number is to big {number}");
             }
 
         }
 
-        public static BigInteger DisplayNumber(BigInteger n)
+        public static BigInteger DisplayNumber(BigInteger number)
         {
-            if (n == 0)
-                return 1;
-            else
-                RuntimeHelpers.EnsureSufficientExecutionStack();
-            return n * DisplayNumber(n - 1);
-        }
+            BigInteger fact = 1;
+            
 
+                while (number != 0)
+                {
+                    fact *= number;
+                    number -= 1;
+                }
+            
+           
+            return fact;
+        }
     }
 }
 
